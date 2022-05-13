3a9dd4bf200c59f275f9830883b8770d7e719516
Ivo Herweijer <info@edwhs.nl>
2022-05-13 14:53:21 +0200

    Added scripts for Rails/Devise test application

---
+++ scripts/rails_devise/analyze.R
@@ -0,0 +1,213 @@
+options(width = 150, echo = FALSE)
+
+# Name (folder) of the app
+app_name    <- "rails_devise"
+
+# Cutoff to determine count of 'slow' responsetimes in milliseconds
+slow_cutoff <- 7.5
+
+# Scale of Y axis for full plots
+ylim_full   <- c(0, 25)
+
+# Scale of Y axis for detailed plots
+ylim_detail <- c(2.5, 12.5)
+
+# Dimensions for all plots
+width       <- 1920
+height      <- 1080
+
+# Load data from csv
+data <- read.csv(
+  file   = paste0("data/", app_name, "/measurements.csv"),
+  header = TRUE,
+  sep    = ",")
+
+data$version <- as.factor(data$version)
+versions <- sort(unique(data$version))
+
+# Print some statistics, to be read by the Ruby script
+cat("\nSlow request counts\n")
+aggregate(y ~ version, data = data[data$y >= slow_cutoff, ], length)
+
+cat("\nMeans\n")
+half_x <- max(data$x) / 2
+aggregate(y ~ version, data = data[data$x > half_x, ], mean)
+
+cat("\nMedians\n")
+aggregate(y ~ version, data = data[data$x > half_x, ], median)
+
+cat("\nCount\n")
+aggregate(y ~ version, data = data, length)
+
+cat("\nSD\n")
+aggregate(y ~ version, data = data, sd)
+
+cat("\n")
+
+# Get name and value of lowest median
+mm <- aggregate(y ~ version, data = data[data$x > half_x, ], median)
+lowest_median <- min(mm$y)
+lowest_median_name <- as.character(mm[mm$y == lowest_median, ]$version)
+
+# Boxplot showing all rubies
+png(
+  file   = paste0("data/", app_name, "/plots/", app_name, "_0_overview.png"),
+  width  = width,
+  height = height)
+
+lim_y <- ceiling(sort(data$y)[length(data$x) * 0.99])
+boxplot(y ~ version, data = data, ylim = c(0, lim_y), pch = 19, cex = 0.2)
+abline(a = lowest_median, b = 0, col = "red", lwd = 3)
+text(
+  1,
+  0,
+  paste0(
+    "Lowest median time [x > ",
+    half_x,
+    "] for ",
+    lowest_median_name,
+    ": ",
+    round(lowest_median, 2),
+    "ms"),
+  pos = 4,
+  col = "black")
+
+# One 'full' scale plot per Ruby
+for (i in seq_len(length(versions))) {
+  df <- data[data$version == versions[i], ]
+
+  png(
+    file = paste0(
+      "data/",
+      app_name,
+      "/plots/",
+      app_name,
+      "_1_",
+      df[1, 1],
+      ".png"),
+    width  = width,
+    height = height)
+
+  plot(
+    df$y ~ df$x,
+    pch  = 19,
+    cex  = 0.2,
+    ylim = ylim_full,
+    col  = df$uri,
+    xlab = "Run",
+    ylab = "milliseconds",
+    main = df[1, 1])
+
+  text(
+    half_x,
+    ylim_full[2],
+    paste(
+      length(df[df$y > ylim_full[2], ]$y),
+      "measurements[ y >",
+      ylim_full[2],
+      "]"),
+    pos = 1,
+    col = "blue")
+
+  a <- mean(df[df$x > half_x, ]$y)
+  if (is.finite(a)) {
+    text(
+      0,
+      ylim_full[1],
+      paste0("Mean time [x > ", half_x, "]: ", round(a, 2), "ms"),
+      pos = 4,
+      col = "black")
+
+    abline(a = a, b = 0, col = "black", lwd = 3)
+  }
+
+  if (is.finite(lowest_median)) {
+    text(
+      half_x,
+      ylim_full[1],
+      paste0(
+        "Lowest median time [x > ",
+        half_x,
+        "] for ",
+        lowest_median_name,
+        ": ",
+        round(lowest_median, 2),
+        "ms"),
+      pos = 4,
+      col = "red")
+
+    abline(a = lowest_median, b = 0, col = "red", lwd = 3)
+  }
+
+  # Set value to TRUE if you want add vertical lines
+  # indicating a major garbage collection run
+  if (FALSE) {
+    gc <- df$x[df$mgc == 1]
+    if (length(gc) <= 50) {
+      for (i in seq_len(length(gc))) {
+        abline(v = gc[i], col = "blue", lwd = 0.5)
+      }
+    }
+  }
+
+  # Save the plot
+  dev.off()
+}
+
+# One detail plot per Ruby
+for (i in seq_len(length(versions))) {
+  df <- data[data$version == versions[i], ]
+
+  png(
+    file = paste0(
+      "data/",
+      app_name,
+      "/plots/",
+      app_name,
+      "_2_",
+      df[1, 1],
+      ".png"),
+    width  = width,
+    height = height)
+
+  plot(
+    df$y ~ df$x,
+    pch  = 19,
+    cex  = 0.2,
+    ylim = ylim_detail,
+    col  = df$uri,
+    xlab = "Run",
+    ylab = "milliseconds",
+    main = df[1, 1])
+
+  text(
+    half_x, ylim_detail[2],
+    paste(
+      length(df[df$y > ylim_detail[2], ]$y),
+      "measurements[ y > ",
+      ylim_detail[2],
+      "]"),
+    pos = 1,
+    col = "blue")
+
+  if (is.finite(lowest_median)) {
+    text(
+      half_x,
+      ylim_detail[1],
+      paste0(
+        "Lowest median time [x > ",
+        half_x,
+        "] for ",
+        lowest_median_name,
+        ": ",
+        round(lowest_median, 2),
+        "ms"),
+      pos = 4,
+      col = "red")
+
+    abline(a = lowest_median, b = 0, col = "red", lwd = 3)
+  }
+
+  # Save the plot
+  dev.off()
+}
---
+++ scripts/rails_devise/measure.rb
@@ -0,0 +1,164 @@
+# frozen_string_literal: true
+
+# All logic specific to the application being tested goes here.
+# The 'Measure' module is included in the main script,
+# so all instance variables of the main script may be used.
+module Measure
+
+  # Hostname and port
+  MEASURE_HOST = '127.0.0.1'
+  MEASURE_PORT = 3000
+
+  # Read / open timeouts in seconds
+  NET_TIMEOUTS = [10, 10]
+
+  private
+
+  # Shell command to determine the process-id (pid) of the application server
+  def measurement_server_pid
+    # Getting the puma or rackup process seems most reliable way of determining the server pid.
+    #
+    # An alternative could be the pid associated with port 9292:
+    # pid = `lsof -ti:3000`.strip
+    #
+    pid = `ps -ef | awk 'match($0,/puma/) && $8!="awk" && $8!="sh" {print $2}'`.strip
+    pid == '' ? nil : pid
+  end
+
+  # Shell command to remove Gemfile.lock file
+  def cmd_remove_application_gemfile_lock
+    'rm -f Gemfile.lock;'
+  end
+
+  # Shell command to switch to the application folder
+  def cmd_switch_to_application_folder
+    "cd apps/#{@app_name} > /dev/null;"
+  end
+
+  # Shell command to bundle gems
+  def cmd_application_bundle_install
+    'bundle install > /dev/null;'
+  end
+
+  # Shell command to start the server process in the background, so end with &
+  def cmd_measurement_run_server(jit)
+    if jit
+      "RUBYOPT=\"#{jit}\" bin/rails server > /dev/null 2>&1 &"
+    else
+      "bin/rails server > /dev/null 2>&1 &"
+    end
+  end
+
+  # Setup tests: set counters and determine uri's
+  def measurement_prepare
+    @uris          = [] # Uri's to test
+    @error_count   = 0
+    @success_count = 0
+    @total_time    = 0.0
+
+    @uris << Net::HTTP::Get.new("http://#{MEASURE_HOST}:#{MEASURE_PORT}/")
+
+    @uris << Net::HTTP::Get.new("http://#{MEASURE_HOST}:#{MEASURE_PORT}/users/sign_up")
+
+    @uris << Net::HTTP::Get.new("http://#{MEASURE_HOST}:#{MEASURE_PORT}/users/password/new")
+
+    # Bad cookie
+    @uris << begin
+      uri = Net::HTTP::Get.new("http://#{MEASURE_HOST}:#{MEASURE_PORT}/authenticated")
+      uri['Cookie'] = "_rails_devise_session=fTx77RlCaR3dmefOV%2FWP8IaKEROVdi1xPaTm69kxRV19NPgpi15BMXRLK%2B69sNaAW%2BELw3XQiJc9N6NaN5JaVTzcnI8%2FaZL0wmOQ%2Bdo3oyZrvGz9iltpC3lyHwWbyZ26YnYMCRtytGsbAwsEh%2BzRETYWn%2F6IVRQnlDWYOkt1lXr5S2Feg9n8W5sr7rQEKR%2BexodfNcpvUsy3cMwC%2BWqabgqgeupIa4E5shZumQfb4%2FePiNsSJuGpi%2FA5Vane05UjJ6Z3JnFJCpieakZSyp8XETxbCz%2F7eClJjisqz5jbhKDqkXy4gC%2FMPdXUyKbuOiHuMbyx1iHlunS1Ah9ADLiv5ynM8N9b2gNb10Luo0YztelF2Oe3riIfrEUvICiO2f4s3vWS7gtUHqtqsysJyQ%3D%3D--52YUKi8hw5UKkUF4--0QLZdS6i8sUz2jZLGga7Rw%3D%3D"
+      uri
+    end
+
+    # Good cookie
+    @uris << begin
+      uri = Net::HTTP::Get.new("http://#{MEASURE_HOST}:#{MEASURE_PORT}/authenticated")
+      uri['Cookie'] = "_rails_devise_session=fTx77RlCaR3dmefOV%2FWP8IaKEROVdi1xPaTm69kxRV19NPgpi15BMXRLK%2B69sNaAW%2BELw3XQiJc9N6NaN5JaVTzcnI8%2FaZL0wmOQ%2Bdo3oyZrvGz9iltpC3lyHwWbyZ26YnYMCRtytGsbAwsEh%2BzRETYWn%2F6IVRQnlDWYOkt1lXr5S2Feg9n8W5sr7rQEKR%2BexodfNcpvUsy3cMwC%2BWqabgqgeupIa4E5shZumQfb4%2FePiNsSJuGpi%2FA5Vane05UjJ6Z3JnFJCpieakZSyp8XETxbCz%2F7eClJjisqz5jbhKDqkXy4gC%2FMPdXUyKbuOiHuMbyx1iHlunS1Ah9ADLiv5ynM8N9b2gNb10Luo0YztelF2Oe3riIfrEUvICiO2f4s3vWS7gtUHqtqsysJyQ%3D%3D--52YUKi8hw5UKkUF4--0QLZdS6i8sUz2jZLGga7Rx%3D%3D"
+      uri
+    end
+  end
+
+  # Execute tests: open connection to the server, run tests, show a progress-bar
+  def measure_run(version)
+    _measure_get_ruby_version
+    @reported_ruby_version ||= version.full_name
+
+    bar = ProgressBar.create(title: 'Testing', format: '%t %a %j% |%B| %c/%C', total: @n)
+
+    Net::HTTP.start(MEASURE_HOST, MEASURE_PORT, { read_timeout: NET_TIMEOUTS[0], open_timeout: NET_TIMEOUTS[1] }) do |http|
+      @n.times do |x|
+        _measure_run_uris(x + 1, http)
+        bar.increment unless bar.finished?
+      end
+    end
+
+    bar.finish
+  end
+
+  # Private method to test if the server is alive and
+  # get the Ruby version reported by the server
+  def _measure_get_ruby_version
+    10.times do
+      begin
+        @reported_ruby_version = Net::HTTP.get(URI("http://#{MEASURE_HOST}:#{MEASURE_PORT}/version"))
+        break
+      rescue
+        sleep 1
+      end
+    end
+  end
+
+  # Private method to execute one test per uri and measure response-times
+  def _measure_run_uris(x, http)
+    @uris.each_with_index do |uri, uri_index|
+      # CLOCK_MONOTONIC should work on most platforms
+      # A good alternative would be CLOCK_MONOTONIC_PRECISE
+      # See: https://docs.ruby-lang.org/en/3.1/Process.html#method-c-clock_gettime
+      t1 = Process.clock_gettime(Process::CLOCK_MONOTONIC, :float_millisecond)
+      http.request(uri)
+      t2 = Process.clock_gettime(Process::CLOCK_MONOTONIC, :float_millisecond)
+      y = t2 - t1
+
+      @results       << [uri_index + 1, x, y]
+      @total_time    += y
+      @success_count += 1
+    end
+
+    GC.start if x % 5_000 == 0
+  rescue
+    @error_count +=1
+  end
+
+  # Finish tests: get garbage collection information from the server,
+  # log error-count and mean response-time
+  def measure_finish(version)
+    begin
+      @mgcs = JSON.parse(
+        Net::HTTP.get(URI("http://#{MEASURE_HOST}:#{MEASURE_PORT}/gc"))
+      ).select { |mgc| mgc > 1 }
+    rescue
+      nil
+    end
+
+    if @success_count > 0
+      version.mean        = (@total_time / @success_count.to_f).round(1)
+    end
+    version.error_count = @error_count
+  end
+
+  # Descriptive text to add to the report
+  def measure_readme_description
+    <<~DESCRIPTION
+      This is a test application based on [Ruby-on-Rails](https://rubyonrails.org/),
+      using [Devise](https://github.com/heartcombo/devise) for authentication.
+
+      Performance of this application was tested by continuously sending requests to the server.
+      Five url's were included in the test:
+
+      1. index-page without cookie (black)
+      2. create-account-page (red)
+      3. reset-password-request-page (green)
+      4. index-page with good cookie (blue)
+      5. index-page with bad cookie (cyan)
+    DESCRIPTION
+  end
+end
